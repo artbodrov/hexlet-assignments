b7e892af5279df593b372e4ef0b9babddd1c4c0a
@hexlet/cli <support@hexlet.io>
2023-06-27 12:44:28 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -8,20 +8,23 @@ import java.util.List;
 public class App {
 
     public static boolean scrabble(String first, String second) {
+
         boolean result;
         var counter = 0;
         var one = first.toLowerCase().toCharArray();
         var two = second.toLowerCase().toCharArray();
-        for (var i = 0; i < one.length;) {
+        var temp = two;
+        for (var i = 0; i < one.length; ) {
             for (var j = 0; j < two.length; j++) {
                 if (one[i] == two[j]) {
                     counter++;
+                    two[j] = ' ';
                     break;
                 }
             }
             i++;
         }
-        if (counter == two.length) {
+        if (counter == temp.length) {
             result = true;
         } else {
             result = false;
