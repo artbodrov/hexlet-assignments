ff3e76a33ec273d17b47ecd899223e566f615294
@hexlet/cli <support@hexlet.io>
2023-08-04 16:32:29 +0300

    submit java-spring-ru/intro-to-spring

--- java-spring-ru/intro-to-spring/src/main/java/exercise/WelcomeController.java
+++ java-spring-ru/intro-to-spring/src/main/java/exercise/WelcomeController.java
@@ -14,7 +14,7 @@ public class WelcomeController {
     }
 
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "anonimus") String name) {
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello, %s!", name);
     }
 
