ac4f97741a39d6043ba8c4e615d114c30727bf8d
@hexlet/cli <support@hexlet.io>
2023-08-10 15:33:17 +0300

    submit java-spring-ru/spring-orm

---
+++ java-spring-ru/spring-orm/src/main/java/exercise/controller/PeopleController.java
@@ -0,0 +1,57 @@
+package exercise.controller;
+
+import exercise.model.Person;
+import exercise.dto.PersonDto;
+import exercise.repository.PersonRepository;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequestMapping("/people")
+@RequiredArgsConstructor
+public class PeopleController {
+
+    // Автоматически заполняем значение поля
+    private final PersonRepository personRepository;
+
+    @GetMapping(path = "/{id}")
+    public Person getPerson(@PathVariable long id) {
+        return personRepository.findById(id).orElseThrow();
+    }
+
+    @GetMapping(path = "")
+    public Iterable<Person> getPeople() {
+        return this.personRepository.findAll();
+    }
+
+    // BEGIN
+    @PostMapping(path = "")
+    public void createPerson(@RequestBody PersonDto personDto) {
+        Person person = new Person();
+        person.setFirstName(personDto.getFirstName());
+        person.setLastName(personDto.getLastName());
+        personRepository.save(person);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deletePerson(@PathVariable long id) {
+        this.personRepository.deleteById(id);
+    }
+
+    @PatchMapping(path = "/{id}")
+    public void updatePerson(@PathVariable long id, @RequestBody PersonDto personDto) {
+        Person person = personRepository.findById(id).orElseThrow();
+        person.setFirstName(personDto.getFirstName());
+        person.setLastName(personDto.getLastName());
+        personRepository.save(person);
+    }
+    // END
+}
